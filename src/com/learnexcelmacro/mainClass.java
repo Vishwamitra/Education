b2a237e731eb936bd6b6752ca90ef72dcb2785e7
Vishwamitra <vishwamitra01@gmail.com>
2016-08-02 22:39:04 +0200

    saving main file

---
+++ src/com/learnexcelmacro/mainClass.java
@@ -0,0 +1,20 @@
+package com.learnexcelmacro;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * Created by Vishwa on 02/08/16.
+ */
+public class mainClass {
+
+    public  void main (String[] args){
+
+        calcAge obj = new calcAge();
+        Scanner inputDOB = new Scanner(System.in);
+
+        System.out.print("Enter your date of birth");
+        System.out.print(obj.ageInYears(inputDOB.next);
+
+    }
+}
