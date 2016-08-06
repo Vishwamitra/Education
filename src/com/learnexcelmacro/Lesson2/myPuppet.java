8e3fe49b7fa7697b06cb5e894c746d1a01655175
Vishwamitra <vishwamitra01@gmail.com>
2016-08-06 19:08:58 +0200

    pushed few more learning lessons

---
+++ src/com/learnexcelmacro/Lesson2/myPuppet.java
@@ -0,0 +1,14 @@
+package com.learnexcelmacro.Lesson2;
+
+/**
+ * Created by Vishwa on 8/6/2016.
+ */
+public class myPuppet {
+
+    public myPuppet (String myName, composition obj){
+
+
+
+    }
+
+}
