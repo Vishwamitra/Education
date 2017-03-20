ffa6e52021cf4d40aacede4a425953cfb6ac3e99
Vishwamitra <vishwamitra01@gmail.com>
2017-03-20 21:25:18 +0100

    pushing advanced lessons

---
+++ src/AdvancedJava/ListInJava.java
@@ -0,0 +1,23 @@
+
+
+package AdvancedJava;
+
+/**
+ * Created by Vishwa on 3/20/2017.
+ */
+
+public class ListInJava {
+
+    public  void main (String[] args)
+    {
+    String[] words= {"Ram", "Shyam", "Haram", "Pranam"};
+
+    for (String w: words)
+    {
+        System.out.print(w.indexOf("r"));
+    }
+
+
+    }
+
+}
