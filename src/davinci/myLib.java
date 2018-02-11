16f7fd276880b436d5f637e0d5aa75081ec49155
Vishwamitra <vishwamitra01@gmail.com>
2018-02-11 09:23:25 +0100

    Merged all other Branches in one for creating a better structure

---
+++ src/davinci/myLib.java
@@ -0,0 +1,22 @@
+package davinci;
+
+/**
+ * Created by vmishra on 8/1/2016.
+ */
+public class myLib {
+    public int getTotal(int arr[]) {
+        int total = 0;
+        for (int y : arr )
+            total += y;
+        return total;
+    }
+
+    public int getDat1(int arr[]) {
+        int total = 0;
+        for (int y : arr )
+            total += y;
+        return total;
+    }
+
+
+}
