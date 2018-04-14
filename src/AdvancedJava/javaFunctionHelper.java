2e10a304175eb74c14f36230855370678e262e58
Vishwamitra <vishwamitra01@gmail.com>
2018-04-14 08:57:49 +0200

    All the functions for education purposes

--- src/AdvancedJava/javaFunctionHelper.java
+++
@@ -1,9 +0,0 @@
-package AdvancedJava;
-
-public class javaFunctionHelper {
-
-    public static void main (String[] args){
-        ListInJava listInJava = new ListInJava();
-        listInJava.printVal();
-    }
-}
--- src/com/learnexcelmacro/Lesson2/apples.java
+++
@@ -1,7 +0,0 @@
-package com.learnexcelmacro.Lesson2;
-
-public class apples {
-    public static void main (String[] args){
-        potpie potObject = new potpie(4,5,80);
-    }
-}
